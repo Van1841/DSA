1c002d4895f432a2bf0cb304414996de3e3c973c
Van1841 <119294875+Van1841@users.noreply.github.com>
2025-05-31 17:30:43 +0530

    Time: 2 ms (99.19%) | Memory: 42.6 MB (33.68%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -2,23 +2,22 @@ class Solution {
     public String removeOuterParentheses(String s) {
         // dekh ismei sabhse pehle hme logic dekhna hai so lets consider
         //  "(" --> to be +1 and ")" --> to be -1
+        // String builder se we can append the ans
+        // and count to make sure ki kab konsa char append hona chahiye!
 
         int count = 0;
         StringBuilder ans = new StringBuilder();
 
         for(char c : s.toCharArray()){
-            if(c =='('){
-                if(count != 0){
-                    ans.append(c);
-                }
-                count++;
-            }
-            
+                        
             if(c ==')'){
                 count--;
-                if(count != 0){
-                    ans.append(c);
-                }
+            }
+            if(count != 0){
+                ans.append(c);
+            }
+            if(c =='('){                    //tis would not be valid in string hence CHAR !
+                count++;
             }
         }
         return ans.toString();
