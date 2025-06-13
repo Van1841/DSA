f4b5a9512b504df9805249adffd3a2cc340bac4e
Van1841 <119294875+Van1841@users.noreply.github.com>
2025-06-13 13:59:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (7.83%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,6 +1,6 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
+        /*int n = matrix.length;
         int [][] ans = new int[n][n];
 
         for(int rows = 0; rows < matrix.length; rows++){
@@ -13,6 +13,34 @@ class Solution {
             for(int cols = 0; cols < matrix.length ; cols++){
                 matrix[rows][cols] = ans[n-cols-1][rows];
             }
+        }*/
+
+        
+        // firstly we will transpose the matrix
+        //keep in mind for first for loop of i, we have to go till length-2 times
+        for(int i=0;i<matrix.length-1;i++){
+            for(int j=i+1;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        //now just need to reverse each row of the matrix.
+        for(int i=0;i<matrix.length;i++){
+            reverse(matrix,i);
+        }
+    }
+    public void reverse(int[][] arr,int row){
+        int i=0;
+        int j=arr[0].length-1;
+        while(i<j){
+            int temp=arr[row][i];
+            arr[row][i]=arr[row][j];
+            arr[row][j]=temp;
+            i++;
+            j--;
         }
     }
 }
+
+    
