fcde2361d60b46fbdb22a7d51181413104873d4e
Vanshika Bhojani <119294875+Van1841@users.noreply.github.com>
2025-07-27 19:00:10 +0530

    Time: 2 ms (93.15%) | Memory: 55.9 MB (52.13%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0; 
+        int ten = 0;
+        // we dont keep 20 as usmei kuch return nhi krna hai hme ok??
+
+        for(int i = 0; i < bills.length; i++){
+            if(bills[i] == 5){
+                five = five + 1;
+            }else if (bills[i] == 10 && five > 0){
+                ten = ten + 1;
+                five = five-1;
+            }else{         // user ne 20 diya hua hai !
+                if(ten > 0 && five > 0){
+                    ten = ten -1;
+                    five = five -1;
+                }else if(five >= 3){
+                    five = five -3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return (five >= 0 && ten >= 0)? true:false;
+    }
+}
