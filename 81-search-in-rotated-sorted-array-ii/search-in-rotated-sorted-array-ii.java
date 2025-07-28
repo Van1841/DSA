30c2bf39f34ff0a7c651a3c6fd647bd157e7d017
Vanshika Bhojani <119294875+Van1841@users.noreply.github.com>
2025-07-28 19:59:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (36.85%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        // pehle sorted half nikal left wala ya phir right wala and usse eliminate krde.
+
+        int left = 0, right = nums.length -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(target == nums[mid]) return true;
+
+             if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+                continue;
+            }
+            
+            // check if left half is sorted !
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && nums[mid] >= target){
+                    right = mid -1;
+                }else{
+                    left = mid +1;
+                }
+            }else{
+                if(nums[mid] <= target && nums[right] >= target){
+                    left = mid+1;
+                }else{
+                    right = mid -1;
+                }
+            } 
+        }
+        return false;
+    }
+}
