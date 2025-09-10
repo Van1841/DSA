09d1fb9e7d99b83f0824d8d1037ba2516ca9abb8
Vanshika Bhojani <119294875+Van1841@users.noreply.github.com>
2025-09-10 21:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (50.84%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return fn(root, Long.MIN_VALUE , Long.MAX_VALUE);
+    }
+    private boolean fn (TreeNode r, long min, long max){
+        if (r == null) return true;
+
+        if (!(r.val > min && r.val < max)) return false;
+
+        return fn(r.left,min,r.val) && fn(r.right,r.val,max);
+    }
+}
